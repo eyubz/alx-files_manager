0a1ff8b83b53822a27f7b25884ad945e576d6ba8
Bz14 <bezueyerusalem@gmail.com>
2024-12-08 11:06:33 +0300

    Files updated.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,11 @@
 import sha1 from 'sha1';
+import Queue from 'bull';
 import db from '../utils/db';
+
+/**
+ * UsersController
+ */
+const userQueue = new Queue('userQueue');
 
 class UsersController {
   static async postNew(req, res) {
@@ -14,6 +20,9 @@ class UsersController {
     let newUser;
     try {
       newUser = await db.users.insert({ email, password: hashedPassword });
+      await userQueue.add({
+        userId: newUser.insertedId.toString(),
+      });
     } catch (error) {
       return res.status(500).send({ error: 'Error creating user' });
     }

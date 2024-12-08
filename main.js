4ef414e51413caa7c681ad8fc4bf4f694444b562
Bz14 <bezueyerusalem@gmail.com>
2024-12-08 09:44:30 +0300

    Files updated.

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-import redisClient from "./utils/redis";
+import dbClient from "./utils/db";
+
+const waitConnection = () => {
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    const repeatFct = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Wrap setTimeout in a promise
+      i += 1;
+      if (i >= 10) {
+        reject(new Error("Max retries reached"));
+      } else if (!dbClient.isAlive()) {
+        repeatFct();
+      } else {
+        resolve();
+      }
+    };
+    repeatFct();
+  });
+};
 
 (async () => {
-  console.log(redisClient.isAlive());
-  console.log(await redisClient.get("myKey"));
-  await redisClient.set("myKey", 12, 5);
-  console.log(await redisClient.get("myKey"));
-
-  setTimeout(async () => {
-    console.log(await redisClient.get("myKey"));
-  }, 10);
+  console.log(dbClient.isAlive());
+  await waitConnection();
+  console.log(dbClient.isAlive());
+  console.log(await dbClient.nbUsers());
+  console.log(await dbClient.nbFiles());
 })();
